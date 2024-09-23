771d227fdeb176c1d8c3c429f7dc63716852939b
Rikuuzen <rk01@outlook.fr>
2024-09-23 22:42:19 +0200

    code avec email js

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -12,30 +12,9 @@ use Symfony\Component\Mime\Email;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function contact(Request $request, MailerInterface $mailer): Response
+    public function contact(): Response
     {
-        $form = $this->createForm(ContactType::class);
-
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
-
-            // Créer et envoyer l'e-mail
-            $email = (new Email())
-                ->from('arcadia.proinfo@gmail.com') // Ton adresse e-mail
-                ->to('arcadia.proinfo@gmail.com') // Adresse de réception
-                ->subject($data['title'])
-                ->text($data['description'] . "\n\nContact: " . $data['email']);
-
-            $mailer->send($email);
-
-            // Ajouter un message flash et rediriger
-            $this->addFlash('success', 'Votre message a été envoyé avec succès.');
-            return $this->redirectToRoute('contact');
-        }
-
         return $this->render('contact/contact.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
